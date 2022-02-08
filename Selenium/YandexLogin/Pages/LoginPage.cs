7317d16fe08302965e568d81d153d32b35f2bf80
nkostovltlife <nkostov@lt.life>
2022-02-08 15:26:58 +0200

    Add Task20

---
+++ Selenium/YandexLogin/Pages/LoginPage.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+
+namespace YandexLogin.Pages
+{
+    public class LoginPage : BasePage
+    {
+        private By usernameInputFieldLocator = By.Id("passp-field-login");
+        private By passwordInputFieldLocator = By.Id("passp-field-passwd");
+        private By loginButtonLocator = By.Id("passp:sign-in");
+        private By loggedUserLocator = By.XPath("//span[@class = 'username desk-notif-card__user-name']");
+
+        public LoginPage(IWebDriver driver) : base(driver)
+        {
+
+        }
+
+        public IWebElement usernameInputField => Driver.FindElement(usernameInputFieldLocator);
+        public IWebElement passwordInputField => Driver.FindElement(passwordInputFieldLocator);
+        public IWebElement loginButton => Driver.FindElement(loginButtonLocator);
+
+        public void LoginButtonClickAndWaitPasswordField()
+        {
+            loginButton.Click();
+            Wait.Until(drv => drv.FindElement(passwordInputFieldLocator));
+        }
+
+        public void LoginButtonClickAndWaitMainPageLoaded()
+        {
+            loginButton.Click();
+            Wait.Until(drv => drv.FindElement(loggedUserLocator));
+        }
+    }
+}
